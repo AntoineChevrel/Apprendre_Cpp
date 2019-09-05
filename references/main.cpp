dab5e3f3b56dae0f271eb6f8082c2acf14f455c4
Achevrel <antoine.chevrel@gmail.com>
2019-09-05 11:16:21 +0200

    Exercice 2

--- references/main.cpp
+++ references/main.cpp
@@ -1,26 +1,42 @@
 #include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
-int main()
-{
-    int a = 1;
-    int b = -2;
-    int c = 0;
+void Ajouter(int a, int b ,int c){
+    c= a+b;
+}
+
+void Ajouter1(int a, int b ,int *c){
+    *c= a+b;
+}
+
+void Ajouter2(int a, int b ,int &c){
+    c= a+b;
+}
+
+int main(){
+    int a=1;
+    int b=-2;
+    int c=0;
 
     cout<<"avant l'appel de Ajouter"<<endl;
-
-    cout<<"a= "<<a<<endl;
-    cout<<"b= "<<b<<endl;
-    cout<<"c= "<<c<<endl;
+    cout<<"a="<<a<<endl;
+    cout<<"b="<<b<<endl;
+    cout<<"c="<<c<<endl;
 
     //Appelle la fonction Ajouter à compléter dans le tableau suivant le cas.
 
-    cout<<"après l'appel de Ajouter"<<endl;
+    Ajouter(a,b,c);
+    //Ajouter1(a,b,&c);
+    //Ajouter2(a,b,c);
 
-    cout<<"a= "<<a<<endl;
-    cout<<"b= "<<b<<endl;
-    cout<<"c= "<<c<<endl;
+    cout<<"après l'appel de Ajouter"<<endl;
+    cout<<"a="<<a<<endl;
+    cout<<"b="<<b<<endl;
+    cout<<"c="<<c<<endl;
 
     return 0;
 }
