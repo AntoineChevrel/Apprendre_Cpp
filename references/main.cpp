81ffc81b311d9459227e4c12040f0fcf21cf5a23
Achevrel <antoine.chevrel@gmail.com>
2019-09-05 10:44:59 +0200

    Exercice 2

---
+++ references/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 1;
+    int b = -2;
+    int c = 0;
+
+    cout<<"avant l'appel de Ajouter"<<endl;
+
+    cout<<"a= "<<a<<endl;
+    cout<<"b= "<<b<<endl;
+    cout<<"c= "<<c<<endl;
+
+    //Appelle la fonction Ajouter à compléter dans le tableau suivant le cas.
+
+    cout<<"après l'appel de Ajouter"<<endl;
+
+    cout<<"a= "<<a<<endl;
+    cout<<"b= "<<b<<endl;
+    cout<<"c= "<<c<<endl;
+
+    return 0;
+}
